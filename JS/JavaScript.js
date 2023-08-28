541cfa54942053208087199461679b810a6e432c
Nazir Knuckles <Batking74@gmail.com>
2023-08-28 01:51:50 -0400

    Updated Calculator

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -1,46 +1,57 @@
 const buttons = document.getElementsByTagName('button');
 const currOperand = document.getElementById('current-operand');
 const prevOperand = document.getElementById('previous-operand');
+const operations = document.querySelectorAll('#operation');
 let numOfCalculations = 0;
+let firstNum;
+let operation;
 
 for(let btn of buttons) {
     btn.addEventListener('click', (e) => {
-        if(numOfCalculations === 1) { currOperand.textContent = ''; numOfCalculations--; }
+        if(numOfCalculations == 1) {
+            prevOperand.textContent = currOperand.textContent;
+            currOperand.textContent = '';
+            numOfCalculations--;
+        }
         const value = e.target.textContent;
-        if(value === 'DEL' && currOperand.textContent != '') deleteNum();
+        if(value === 'DEL') deleteNum();
         else if(value === 'AC') {currOperand.textContent = ''; prevOperand.textContent = ''; }
-        else if(value === '=') validate();
-        else { currOperand.textContent += (e.target.textContent).toString(); }
+        else if(value === '=') { calculate(value); }
+        else if(!(isOperation(value))) currOperand.textContent += (e.target.textContent).toString();
     })
 }
 
+function isOperation(value) {
+    let isOperation = false;
+    for(let i = 0; i < operations.length; i++) {
+        if(value === operations[i].textContent) {
+            operation = operations[i].textContent;
+            firstNum = parseInt(currOperand.textContent);
+            currOperand.textContent = '';
+            isOperation = true;
+        }
+    } return isOperation;
+}
+
 function deleteNum() {
-    currOperand.textContent = currOperand.textContent.substring(0, currOperand.textContent.length - 1);
+    if(!(currOperand.textContent === undefined)) {
+        currOperand.textContent = currOperand.textContent.substring(0, currOperand.textContent.length - 1);
+    }
 }
 
-function validate() {
-    if(currOperand.textContent.includes('×')) {
-        const num = calculate('×');
-        currOperand.textContent = num[0] * num[1];
+function calculate(value) {
+    if(!(operation === undefined)) {
+        const result = {
+            '+': (firstNum + parseInt(currOperand.textContent)),
+            '-': (firstNum - parseInt(currOperand.textContent)),
+            '×': (firstNum * parseInt(currOperand.textContent)),
+            '÷': (firstNum / parseInt(currOperand.textContent))
+        }
+        for(let i = 0; i < Object.keys(result).length; i++) {
+            if(operation === Object.keys(result)[i] && value != prevOperand.textContent) {
+                currOperand.textContent = Object.values(result)[i];
+            }
+            else currOperand.textContent = Object.values(result)[i];
+        } numOfCalculations++;
     }
-    else if(currOperand.textContent.includes('÷')) {
-        const num = calculate('÷');
-        currOperand.textContent = num[0] / num[1];
-    }
-    else if(currOperand.textContent.includes('+')) {
-        const num = calculate('+');
-        currOperand.textContent = num[0] + num[1];
-    }
-    else if(currOperand.textContent.includes('-')) {
-        const num = calculate('-');
-        currOperand.textContent = num[0] - num[1];
-    }
-    numOfCalculations++;
-}
-
-function calculate(operator) {
-    const num1 = parseFloat(currOperand.textContent.substring(0, (currOperand.textContent.indexOf(operator))));
-    const num2 = parseFloat(currOperand.textContent.substring((currOperand.textContent.indexOf(operator)) + 1));
-    prevOperand.textContent = currOperand.textContent;
-    return [num1, num2];
 }
